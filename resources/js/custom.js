5a891f9f2e96ac007a48f7eb2e30dce9405425da
Shahidul Islam <sihelal72@gmail.com>
2021-03-20 07:02:15 +0600

    index created

---
+++ resources/js/custom.js
@@ -0,0 +1,22 @@
+(function ($) {
+    "use strict";
+
+    var userAdmin = {
+        
+        commonFunction: function(){
+            // Statement Goes here
+            
+        },
+
+
+        initializ: function() {
+			userAdmin.commonFunction();
+		},
+
+    };
+    $(function() {
+		userAdmin.initializ();
+	});
+
+
+})(jQuery);
